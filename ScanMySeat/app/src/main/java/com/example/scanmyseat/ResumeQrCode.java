abf182eb2514454c95ff315e54a8ad0415874ede
MajeurTest <chuzzle.9@hotmail.fr>
2023-01-15 22:53:47 +0100

    Modification App check ticket

--- ScanMySeat/app/src/main/java/com/example/scanmyseat/ResumeQrCode.java
+++ ScanMySeat/app/src/main/java/com/example/scanmyseat/ResumeQrCode.java
@@ -8,11 +8,16 @@ import android.widget.ImageView;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ResumeQrCode extends AppCompatActivity {
 
@@ -45,31 +50,56 @@ public class ResumeQrCode extends AppCompatActivity {
         numberTicket = getIntent().getStringExtra("ticket");
 
         //Connect to the api to get all informations for the number of the ticket
+        FirebaseFirestore.getInstance().collection("ticket")
+                .document(numberTicket).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            FirebaseFirestore.getInstance().collection("session")
+                                    .document(documentSnapshot.getString("id_session"))
+                                            .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                            movieName.setText(getIntent().getStringExtra("ticket"));
+                                            room.setText("Room n ° 1");
+                                            dateShow.setText(documentSnapshot.getDate("diffusion").toString());
+                                            //check if it is available with the date and hour (access to the cinema 2h hour before the movie
+                                            //until 30 min after the beginning of the movie
+                                            /*
+                                            Conversion des dates pour comparaison
+                                            if (dateshow is today){
+                                                if(hourshow - 2 <= hourshow and hourshow <= hourshow + 1){
+                                                    isValidOrNot.setText("Valid");
+                                                    imageView.setImageResource(R.drawable.valid);
+                                                    tableRow.setBackgroundResource(R.drawable.row_shape_valid);
+                                                } else {
+                                                    isValidOrNot.setText("No Valid");
+                                                    imageView.setImageResource(R.drawable.novalid);
+                                                    tableRow.setBackgroundResource(R.drawable.row_shape_novalid);
+                                                }
+                                            }
+                                             */
+                                            isValidOrNot.setText("Valid");
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            isValidOrNot.setText("Not Valid");
+                                        }
+                                    });
 
-        //display the informations of the ticket
-        movieName.setText(getIntent().getStringExtra("ticket"));
-        room.setText("Room n ° ");
-        dateShow.setText("Date Show");
-        hourShow.setText("Hour Show");
-        isValidOrNot.setText("Valid ?");
+                        //display the informations of the ticket
+
+                        } else {
+                            isValidOrNot.setText("Not Valid");
+                        }
+                    }
+                });
 
 
-        //check if it is available with the date and hour (access to the cinema 2h hour before the movie
-        //until 30 min after the beginning of the movie
-        /*
-        Conversion des dates pour comparaison
-        if (dateshow is today){
-            if(hourshow - 2 <= hourshow and hourshow <= hourshow + 1){
-                isValidOrNot.setText("Valid");
-                imageView.setImageResource(R.drawable.valid);
-                tableRow.setBackgroundResource(R.drawable.row_shape_valid);
-            } else {
-                isValidOrNot.setText("No Valid");
-                imageView.setImageResource(R.drawable.novalid);
-                tableRow.setBackgroundResource(R.drawable.row_shape_novalid);
-            }
-        }
-         */
+
+
+
     }
 
 
